509301044a1431e2dd59cc8c6791024d8e980889
chuckmarshall3 <chuckmarshall3@gmail.com>
2015-03-30 23:17:47 -0600

    re added src files

---
+++ src/Common/Services/EndPointManager.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chuck
+ * Date: 3/30/15
+ * Time: 8:48 PM
+ */
+
+namespace Common\Services;
+
+
+class EndPointManager {
+
+    public function __construction($config=null){
+
+        if($config == null){
+
+//            throw new \InvalidArgumentException(
+//                //__METHOD__.'$config has to be set correctly';
+//            )
+        }
+    }
+
+}
